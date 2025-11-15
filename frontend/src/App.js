238701100c8e4f8bdd31063cdb23f719126e3eff
Aman Agrahari <aman8cse@gmail.com>
2025-11-15 18:23:54 +0530

    added light and dark modes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,15 @@ import Right from './components/right.jsx'
 function App() {
   const[showLeft, setShowLeft] = useState(false);
   const[showRight, setShowRight] = useState(false);
+  const[theme, setTheme] = useState(false);
+
+  document.documentElement.setAttribute("data-theme", theme ? "dark" : "light");
 
   return (
     <>
+    <div className="themeBox" onClick={() => setTheme(!theme)}>Theme
+      <div className="theme"></div>
+    </div>
     {showLeft && (
       <div className="special">
         <Left showLeft={showLeft} onTopClick={()=>setShowLeft(!showLeft)}/>
